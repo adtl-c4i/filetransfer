cabfea55998bb4d96870d9609006efe3ab60ef79
adtl-c4i <adtlsoft@gmail.com>
2026-08-01 00:24:04 +0530

    Add basic SSL plugin and configure build inputs
    Added basic SSL support and updated build configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import { resolve } from "node:path";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "./",
   plugins: [
+    basicSsl(),
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: ["**/*.{html,css,js,wasm,svg,png}"],
@@ -13,7 +17,7 @@ export default defineConfig({
         theme_color: "#121009",
         background_color: "#121009",
         display: "standalone",
-        start_url: "/",
+        start_url: "./index.html",
         icons: [
           {
             src: "https://raw.githubusercontent.com/twitter/twemoji/master/assets/72x72/1f4f6.png",
@@ -31,5 +35,17 @@ export default defineConfig({
         globPatterns: ["**/*.{js,css,html,wasm,png,svg}"]
       }
     })
-  ]
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        send: resolve(__dirname, "send/index.html"),
+        receive: resolve(__dirname, "receive/index.html"),
+        noteSend: resolve(__dirname, "notes/send/index.html"),
+        noteReceive: resolve(__dirname, "notes/receive/index.html"),
+      },
+    },
+  },
+  server: { host: true },
 });
